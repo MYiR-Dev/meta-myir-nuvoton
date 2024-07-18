508d6131532c597275fcaa7c09aa5656381bdd48
beste <1596775839@qq.com>
2024-07-18 13:59:41 +0800

    FEAT:Add timesync configure in startup package

--- recipes-myir/start-service/start-service/startup.sh
+++ recipes-myir/start-service/start-service/startup.sh
@@ -1,31 +1,30 @@
 #!/bin/sh
+
+TIMESYNCD_CONF="/etc/systemd/timesyncd.conf"
+NEW_CONF_CONTENT="[Time]\nFallbackNTP=ntp.ntsc.ac.cn cn.ntp.org.cn time1.google.com time2.google.com time3.google.com time4.google.com"
+
+# 检查是否存在 /etc/systemd/timesyncd.conf 文件
+if [ -f "$TIMESYNCD_CONF" ]; then
+    # 检查文件中是否包含 #MYiR timesyncd conf 内容
+    if grep -q "#MYiR timesyncd conf" "$TIMESYNCD_CONF"; then
+        echo "Already configured, nothing to do."
+        exit 0
+    fi
+
+    # 备份原始文件
+    cp "$TIMESYNCD_CONF" "$TIMESYNCD_CONF.bak"
+
+    # 替换文件内容为新配置
+    sed -i '/\[Time\]/,$d' "$TIMESYNCD_CONF"
+    echo -e "#MYiR timesyncd conf\n$NEW_CONF_CONTENT" >> "$TIMESYNCD_CONF"
+
+    echo "Modified $TIMESYNCD_CONF with custom configuration."
+else
+    echo "$TIMESYNCD_CONF not found. Cannot configure timesyncd."
+    exit 1
+fi
+
 echo "start service" > /var/startservice.log
 
-vimrc_file="/usr/share/vim/vimrc"
-commented_backup=$(grep -E '^\s*".*set backup' "$vimrc_file")
-commented_else=$(grep -E '^\s*".*else' "$vimrc_file")
-
-if [[ -z $commented_backup ]]; then
-	sed -i '/^\s*set backup/s/^/"/' "$vimrc_file"
-	if [[ -z $commented_else ]]; then
-		sed -i '/^\s*else/s/^/"/' "$vimrc_file"
-	fi
-	echo "Lines commented out."
-else
-	echo "Lines already commented out."
-fi
-
-echo "Start Up"
-
-FILENAME="/etc/myir-hmi"
-
-if [ ! -f "$FILENAME" ] || [ "$(cat $FILENAME)" = "0" ]; then
-    echo "driftfile /var/lib/ntp/drift" > /etc/ntp.conf
-    echo "server ntp.aliyun.com iburst" >> /etc/ntp.conf
-    ts_calibrate
-    echo "1" > $FILENAME
-fi
-
-systemctl restart ntpd
-
+echo "Start service" > /dev/ttyS0
 exit 0
